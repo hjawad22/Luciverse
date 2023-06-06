b70ecce1f4379de1e0c272d1a0b121a8eef1fb30
Houda Jawad <houdajawad@gmail.com>
2023-06-06 08:23:57 -0700

    Build out Nav component and add styling to it

---
+++ src/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Nav/Nav.css'
+
+const Nav = () => {
+    return (
+        <nav className="nav-container">
+         <h1>LuciVerse</h1>
+         <h3>ABOUT</h3>
+        </nav>
+    )
+}
+
+
+export default Nav;
