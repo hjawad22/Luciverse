601d2607a96205684d54ba005d8902fd7353fc0d
Houda Jawad <houdajawad@gmail.com>
2023-06-09 09:01:24 -0700

    Add ending semicolons and fix spacing

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,23 +1,23 @@
 import React from "react";
-import '../Nav/Nav.css';
+import "../Nav/Nav.css";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 const Nav = () => {
     return (
         <nav className="nav-container">
             <div className="logo-container">
-            <NavLink style={{ color: 'inherit', textDecoration: 'inherit' }} to={"/"}>
+            <NavLink style={{ color: "inherit", textDecoration: "inherit" }} to={"/"}>
                 <h1 className="logo">LuciVerse</h1>
             </NavLink>
             </div>
             <div className="about-container"> 
-            <NavLink style={{ color: 'inherit', textDecoration: 'inherit' }} to={"/About"}>
+            <NavLink style={{ color: "inherit", textDecoration: "inherit" }} to={"/About"}>
                 <h2 className="about">About</h2>
             </NavLink>
             </div>
         </nav>
-    )
-}
+    );
+};
 
 
 export default Nav;
